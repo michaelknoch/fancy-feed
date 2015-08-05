c133a77a4ba7fea96e45cc85e6b823cefbf712b7
Michael Knoch <info@michaelknoch.de>
2015-08-05 15:30:59 +0200

    refactor strukture

--- src/app/states/instagram/instagramCtrl.js
+++ src/app/states/instagram/instagramCtrl.js
@@ -8,5 +8,13 @@
   /** @ngInject */
   function InstagramCtrl($timeout) {
 
+    var feed = new Instafeed({
+      get: 'tagged',
+      tagName: 'donau2015',
+      clientId: 'bc888d9b72154876aa0ace1a420a2991'
+    });
+    feed.run();
+    console.info('yolo');
+
   }
 })();
