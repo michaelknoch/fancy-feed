480924829a41a89218e2b68fcb4ae566396bdffe
Michael Knoch <info@michaelknoch.de>
2015-08-05 15:04:06 +0200

    add instagram and twitter routes

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   'use strict';
 
   angular
@@ -13,6 +13,18 @@
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('instagram', {
+        url: '/instagram',
+        templateUrl: 'app/main/main.html',
+        controller: 'TwitterCtrl',
+        controllerAs: 'tc'
+      })
+      .state('twitter', {
+        url: '/twitter',
+        templateUrl: 'app/instagram/instagram.html',
+        controller: 'InstagramCtrl',
+        controllerAs: 'ic'
       });
 
     $urlRouterProvider.otherwise('/');
