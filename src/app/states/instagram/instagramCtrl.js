2b5a8dd5c36de6eccf99fcb53cc11a6ddb99ca9f
Michael Knoch <info@michaelknoch.de>
2015-08-21 22:11:48 +0200

    improvments

--- src/app/states/instagram/instagramCtrl.js
+++ src/app/states/instagram/instagramCtrl.js
@@ -6,26 +6,32 @@
     .controller('InstagramCtrl', InstagramCtrl);
 
   /** @ngInject */
-  function InstagramCtrl(InstagramService, $scope) {
+  function InstagramCtrl(InstagramService, $scope, $state) {
 
     $scope.instagrams = [];
 
     InstagramService.getRecentImages('donau2015')
 
       .then(function (result) {
-
         var tmp = [];
 
         result.data.data.forEach(function (item) {
 
           tmp.push({
             path: item.images.standard_resolution.url,
-            name: item.user.full_name
+            name: item.user.full_name,
+            tags: item.tags
           });
 
         });
 
+
         $scope.instagrams = tmp;
+        console.info($scope.instagrams);
+
+        var loop_handle = setTimeout(function () {
+          $state.go('instagram', {}, {reload: true});
+        }, 600000);
 
       })
       .catch(function (result) {
