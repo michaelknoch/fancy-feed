97740a61b39d75661b16948341c2c4719df7b277
Michael Knoch <info@michaelknoch.de>
2015-08-21 22:11:13 +0200

    modify instagram endpoint

--- src/app/services/InstagramService.js
+++ src/app/services/InstagramService.js
@@ -14,7 +14,7 @@
           console.log('tag should be defined');
           return;
         }
-        return $http.get('https://api.instagram.com/v1/tags/' + tag + '/media/recent?access_token=201907240.bc888d9.d5e3829a59c449deb4aa5620ac29bc70')
+        return $http.get('http://api.donau2015.com/instagrams/' + tag);
       }
 
     }
